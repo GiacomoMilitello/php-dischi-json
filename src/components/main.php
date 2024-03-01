0b57c31193242517fabe4f81f71c4189dc023648
GiacomoMilitello <g.militello95@gmail.com>
2024-03-01 13:47:50 +0100

    server.php & json

---
+++ src/components/main.php
@@ -0,0 +1,15 @@
+<main>
+    <div class="container">
+        <div class="row p-4">
+            <div class="col-4">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body text-center">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,17 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            apiUrl: 'server.php'
         }
     },
     mounted() {
     },
     methods: {
+        getDischi(){
+            axios.get( this.apiUrl )
+            .then((response) => {
+                this.dischi = response.data
+            })
+        }
     }
 }).mount('#app')
